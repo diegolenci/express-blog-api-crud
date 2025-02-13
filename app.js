42e2949a7074761e453fe6797ae512a80add670c
diegolenci <lencidiego0@gmail.com>
2025-02-13 14:13:53 +0100

    attivazione server localh host 3000

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+const postRouter = require('./routers/postsRouter')
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Hello boolean');
 })
+
+app.use('api/posts', postRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
