594dc37d6f4a7cf01f0210a4d19e7d839d2d5d47
diegolenci <lencidiego0@gmail.com>
2025-02-13 14:38:14 +0100

    importazione array dei posts

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -2,3 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const postController = require('../controllers/postsController')
+
+router.get('/', postsController.index);
+router.get('/:id', postsController.show);
+router.post('/', postsController.store);
+router.put('/:id', postsController.update);
+router.patch('/:id', postsController.patch);
+router.delete('/:id', postsController.delete);
+
+module.exports = router;
