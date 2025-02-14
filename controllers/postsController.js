ab03d3687b923aef28e370d537083b589ddf2048
diegolenci <lencidiego0@gmail.com>
2025-02-14 14:12:06 +0100

    Add: funzione update

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -29,6 +29,27 @@ function store(req, res) {
 
 function update(req, res) {
     
+    const id = parseInt(req.params.id)
+    const post = posts.find(post => post.id === id)
+
+    if (!post) {
+        res.status(404)
+
+        return res.json(
+            {
+                status: 404,
+                error: "not found",
+                message: 'post not found'
+            }
+        )
+    }
+    post.title = req.body.title,
+    post.content = req.body.content,
+    post.image = req.body.image,
+    post.tags = req.body.tags,
+
+    console.log(posts)
+    res.json(post)
 }
 
 function patch(req, res) {
